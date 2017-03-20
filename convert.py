c4f83ee7e6b010ffcbfb150f2a20eb10f2113b70
Shubham mittal <upgoingstaar@gmail.com>
2017-03-21 01:56:48 +0530

    Removed extra argument for output file.

--- convert.py
+++ convert.py
@@ -1,15 +1,20 @@
 import json
 from json2html import *
 import argparse
+
 parser=argparse.ArgumentParser()
 parser.add_argument('filename' ,help='Enter the json file')
-parser.add_argument('output_filename' ,help='Enter the name of output file with html extension')
 args=parser.parse_args()
+
+output_filename = str(args.filename.split('.')[0]) + '.html'
+print output_filename
+
+
 with open(args.filename) as json_data:
     d=json.load(json_data)
     f=json2html.convert(json=d)
     print('<pre>' + f + '</pre> <br>\n')
-    with open(args.output_filename, "w") as e: 
+    with open(output_filename, "w") as e: 
         e.write("<pre>" + f + "</pre> <br>\n")
         e.close()
-        
+
